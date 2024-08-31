c528fc76cfea84fcf5b1864ddfa1e029f8e3bba5
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2024-08-31 14:21:33 +0530

    data added in to conversation file

---
+++ 2-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi suresh, How are you?"
+echo "Suresh:: Hello Ramesh, I am fine. how are you doing"
+echo "Ramesh:: I am doing good suresh, what is going on?"
+echo "suresh:: I Started learnin shell scripting Ramesh"
