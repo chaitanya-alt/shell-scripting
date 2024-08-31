3fccbe8af459283875eee01ec2c28c41ab13d7de
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2024-08-31 14:25:43 +0530

    data added in to variable files

---
+++ 3-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1:$1
+PERSON2:$2
+
+echo "$PERSON1:: Hi $PERSON2 sh, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. how are you doing"
+echo "$PERSON1:: I am doing good $PERSON2, what is going on?"
+echo "$PERSON2:: I Started learnin shell scripting $PERSON1"
