35ab58e0faa1117fbdf7729a5135812d6a1f968f
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2024-08-31 14:49:43 +0530

    data added in to variable-5 files

---
+++ 5-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+echo "Please enter your usernae::"
+
+read -s USERNAME  #takes input in to USERNAME variable
+
+echo "entered username is : $USERNAME"
+
+echo "please enter your password::"
+
+read -s PASSWORD
+
+echo "entered password is $PASSWORD"
