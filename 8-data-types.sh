4eb51b3c237a3da0174daf4e8e378b80d54c665e
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2024-09-01 06:57:59 +0530

    the data is added in to data types file

--- 8-data-types.sh
+++ 8-data-types.sh
@@ -3,6 +3,6 @@
 NUMBER1=$1
 NUMBER2=$2
 
-SUM=$(($NUMBER!+$NUMBER2))  # Outside brackets is for shell scripting and inside brackets is for addition.
+SUM=$(($NUMBER1+$NUMBER2))  # Outside brackets is for shell scripting and inside brackets is for addition.
 
 echo "The sum amount is : $SUM"
