850e92f1aa34257abe157fb4fa278ab3416945e6
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2024-09-01 12:33:33 +0530

    the data is added in to colors file

---
+++ 15-colors.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+
+#!/bin/bash
+
+# check the user has root access or not.
+# if root access, proceed with script.
+# otherwise throw the error.
+# check already install or not if yes, inform user it is already installed.
+# if not install it.
+# check if it is success or not.
+
+
+# echo -e "\e[31m Hello World" ---> it is syntax in linux
+
+#sudo is root user
+
+USERID=$(id -u)  #you need to run script with sudo
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+    echo "Please run the script with root previliges"
+    exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo -e "$2 is $R FAILED..$N"
+        exit 1
+
+    else
+        echo -e "$2 is $GSUCCESS..$N"
+    fi
+}
+
+
+dnf list install git
+
+VALIDATE $? "git installation"
+
+echo "user id is:$USERID"
+
+CHECK_ROOT
+
+
+dnf list install git
+
+if [ $? -ne 0 ]
+then
+    echo "git is not install, we are going to instal it"
+    dnf install git -y
+    VALIDATE $? "Installing git"
+else 
+    echo "git is already installed, nothing to do"
+fi
+
+##mysql
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+    echo "mysql is not installed, we are goint to install it..."
+    dnf install mysql -y
+    VALIDATE $? "Installing mysql"
+else
+    echo "mysql is already installed, nothing to do..."
+fi
+
+
+
+
