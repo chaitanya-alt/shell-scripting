31178085d46632e09670f4c0c9406b12ddf5df8b
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2024-09-01 16:28:03 +0530

    the data is added in to loops file

--- 16-loops.sh
+++ 16-loops.sh
@@ -1,5 +1,5 @@
 #!/in/bash
-
+#below is manuale
 # echo "1"
 # echo "2"
 # echo "3"
@@ -11,7 +11,13 @@
 # echo "9"
 # echo "10"
 
-for i in 1 2 3 4 5 6 7 8 9 10 
+
+# for i in 1 2 3 4 5 6 7 8 9 10  # one of the process
+# do
+#     echo $i
+# done
+
+for i in {0..10}
 do
-    echo $i
+    echo $I
 done
