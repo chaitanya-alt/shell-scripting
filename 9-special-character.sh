a2e7da105096f032f8389d0b17f1d493d8e9f39a
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2024-09-01 07:09:52 +0530

    the data is added in to special characters file

--- 9-special-character.sh
+++ 9-special-character.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "All variables passed to script :$@"
+echo "All variables passed to script :$@" #It will print what ever you gave as arguments
+echo "Number variable passed to scrippt : $#"
+echo "Script name is:$0"
+echo "current directory :$PWD"
